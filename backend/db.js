158e3f89be3ecf2d8a921372e2261ee402321304
Ashwin <ashwinsanjeev33@gmail.com>
2025-08-12 21:13:19 +0530

    Deploy

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 
 const client=new Client({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'postgres',
-    port: process.env.DB_PORT || 5432,
-    password: process.env.DB_PASSWORD || '0311',
-    database: process.env.DB_NAME || 'blood_donor_connect'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 })
 
 client.connect()
