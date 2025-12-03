eaabfbba4c60d2fce85c49270d058fbd1ee84090
ashwinsanjeev <167792578+ashwinsanjeev@users.noreply.github.com>
2025-12-03 15:06:44 +0530

    Update API endpoint for donor registration

--- frontend/js/registerDonor.js
+++ frontend/js/registerDonor.js
@@ -33,7 +33,7 @@ async function registerDonor() {
     }
     try {
         // Send data to the backend
-        const response = await fetch('http://13.233.17.238:5050/api/donors', {
+        const response = await fetch('http://api-bloodconnect.duckdns.org/api/donors', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(donorData),
